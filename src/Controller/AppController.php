46cb8667df04edbd51f06215d660144cf7862d98
Paulo Henrique <paulo.henrik.oliver@gmail.com>
2023-07-06 09:56:27 -0300

    public function publicidade update

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -167,9 +167,9 @@ class AppController extends Controller
 
     public function publicidade(){
         $this->loadModel('Publicidades');
-        $publicidadeTopo = $this->Publicidades->find()->where(['local' => 1])->order('rand()')->limit(1);
-        $publicidadeLateral = $this->Publicidades->find()->where(['local' => 2])->order('rand()')->limit(1);
-        $publicidadeAbaixoBannerPrincipal = $this->Publicidades->find()->where(['local' => 3])->limit(1);
+        $publicidadeTopo = $this->Publicidades->find()->where(['local' => 1])->order('rand()')->order('rand()')->limit(1);
+        $publicidadeLateral = $this->Publicidades->find()->where(['local' => 2])->order('rand()')->order('rand()')->limit(1);
+        $publicidadeAbaixoBannerPrincipal = $this->Publicidades->find()->where(['local' => 3])->order('rand()')->limit(1);
         $this->set(compact('publicidadeTopo', 'publicidadeLateral','publicidadeAbaixoBannerPrincipal'));
     }
 
